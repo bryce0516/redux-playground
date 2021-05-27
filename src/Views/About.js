373eed58719214f1a55fb76f63082572f051fcd8
Hyesung Park <51251258+bryce0516@users.noreply.github.com>
2021-05-27 17:44:53 +0900

    usecallback usememo add

--- src/Views/About.js
+++ src/Views/About.js
@@ -1,8 +1,52 @@
+import React, { useState, useCallback } from "react";
+import Info from "../utils/Info";
 const About = () => {
-  console.log("this is about");
+  const [color, setColor] = useState("");
+  const [movie, setMovie] = useState("");
+  const onChangeHandler = useCallback((e) => {
+    if (e.target.id === "color") setColor(e.target.value);
+    else setMovie(e.target.value);
+  }, []);
+
   return (
-    <div>
-      <p>this is About</p>
+    <div style={{ flexDirection: "column" }}>
+      <div>
+        <label>
+          What is your favorite color of rainbow ?
+          <input id="color" value={color} onChange={onChangeHandler} />
+        </label>
+      </div>
+      <div>
+        What is your favorite movie among these ?
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="Marriage Story"
+            onChange={onChangeHandler}
+          />
+          Marriage Story
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="The Fast And The Furious"
+            onChange={onChangeHandler}
+          />
+          The Fast And The Furious
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="Avengers"
+            onChange={onChangeHandler}
+          />
+          Avengers
+        </label>
+      </div>
+      <Info color={color} movie={movie} />
     </div>
   );
 };
--- src/Views/Counter.js
+++ src/Views/Counter.js
@@ -1,25 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { increment } from "../redux/Counter";
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
   const incrementState = () => {
-    setCounter((prevCounter) => prevCounter + 1);
+    setCounter(counter + 1);
   };
   const dispatch = useDispatch();
   const count = useSelector((state) => state);
-  console.log("this is count useselector", count);
-
-  const memoizedCallback = useCallback(() => {
-    callback;
-  }, [input]);
+  const handleClick2 = useCallback(
+    () => console.log("clicked!" + counter),
+    [counter]
+  );
+  const handleClick1 = () => console.log("clicked!" + counter);
+  // useCallback(() => {
+  //   console.log("this is count useselector", counter);
+  // }, [counter]);
+  // console.log("this is count useselector" + counter);
   return (
     <div>
       <p>this is counter</p>
       <div>
         Value: {counter} <button onClick={incrementState}>Increment</button>
       </div>
+      <button onClick={handleClick2}>클릭해보세요!</button>
       <p>this is reudx counter</p>
       <div>
         Value: {count.value}{" "}
